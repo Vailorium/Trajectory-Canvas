5150f893fd7a113966c29372359da75da7c95429
Samuel Hembrow <47310940+Vailorium@users.noreply.github.com>
2019-10-17 10:21:43 +1300

    basic trajectory calculation (kinematics)

--- scripts.js
+++ scripts.js
@@ -1,5 +1,5 @@
 var c;
-
+var context; 
 function relMouseCoords(event){
     var totalOffsetX = 0;
     var totalOffsetY = 0;
@@ -20,25 +20,57 @@ function relMouseCoords(event){
 }
 HTMLCanvasElement.prototype.relMouseCoords = relMouseCoords;
 
-function getMousePos(canvas, evt) {
-    var rect = canvas.getBoundingClientRect();
-    return {
-    x: evt.clientX - rect.left,
-    y: evt.clientY - rect.top
-    };
-}
+
+var horzSpeed = 10;
+
+var clickedLocation = 0;
+
+var playerLoc = {x: 1000, y: 1000}
+var points = [];
+var accuracy = 10;
 
 $(document).ready(function(){
     c = document.getElementById("mainCanvas");
     context = c.getContext("2d");
     $("#mainCanvas").click(function(ev){
-        context = c.getContext("2d");
-        context.beginPath();
-        context.moveTo(50.5, 50);
+        clickedLocation = this.relMouseCoords(ev).x * 4;
 
-        var mousePos = getMousePos(c, ev);
-        console.log(mousePos);
-        context.lineTo(this.relMouseCoords(ev).x, this.relMouseCoords(ev).y);
-        context.stroke();
+        drawLine();
     });
 });
+
+function drawLine(){
+    context = c.getContext("2d");
+    context.clearRect(0, 0, 2000, 2000);
+    context.beginPath();
+    context.moveTo(1000.5, 1000);
+
+    var goTo = clickedLocation;
+    var current = Object.assign({}, playerLoc);
+    var increment = (goTo - playerLoc.x) / accuracy;
+    var time = Math.abs(goTo - playerLoc.x) / horzSpeed;
+
+    // time / 2 is when vert velocity must = 0
+
+    var intialVelocity = -9.81 * (time / 2);
+    var incrementTime = time / accuracy;
+
+    // 0 = vi + a*t
+    // vi = - a*t
+
+    for(var i = 1; i < accuracy; i++){
+        current.x += increment;
+        current.y += intialVelocity - (-9.81 * incrementTime * i);
+        context.lineTo(current.x, current.y);
+        context.stroke();
+    }
+    clickedLocation * 4 < 1000 ? numberthing = -1 : numberthing = 1;
+    context.lineWidth = 10;
+    // context.lineTo(this.relMouseCoords(ev).x * 4, 1000); //this.relMouseCoords(ev).y * 4
+    context.stroke();
+}
+function changeHorzSpeed(value){
+    horzSpeed = value;
+    document.getElementById("test").innerHTML = horzSpeed;
+    drawLine();
+}
